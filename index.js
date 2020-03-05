a09e1213c06e8f7599dfb297f0a18493d03b3079
monikamohan98gmailcom <monikamohan98@gmail.com>
2020-03-05 10:12:50 +0530

    1 file modified

--- index.js
+++ index.js
@@ -37,18 +37,23 @@ console.log(name)  //output=Hipple
 object.setName('Monika')
 const setname=object.getName().title;
 console.log(setname) //output=Monika
+
 object.setTitle('The end')
 const setname=object.getName().title;
 console.log(setname) //output=The end
+
 object.setYear(2019)
 const setname=object.getName().year;
 console.log(setname) //output=2019
+
 object.setPublishingStatus('Super')
 const setname=object.getName().publish_status;
 console.log(setname) //output=Super
+
 object.setreview('Awesome')
 const setname=object.getName().review;
 console.log(setname) //output=Awesome
+
 object.setFamousBooks('The legacy')
 const setname=object.getName().Famous_books;
 console.log(setname) //output=The legacy
