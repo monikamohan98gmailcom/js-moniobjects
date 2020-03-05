2c5815f102ee22ffffd6ac005fa449f0ca5e7e82
monikamohan98gmailcom <monikamohan98@gmail.com>
2020-03-05 10:10:57 +0530

    1 file modified

--- index.js
+++ index.js
@@ -13,6 +13,21 @@ const object={
   //setting the name with the arg value
   setName:function(name){ 
     this.title=name
+  },
+  setTitle:function(title){
+    this.title=title
+  },
+  setYear:function(year){
+    this.year=year
+  },
+  setPublishingStatus:function(ps){
+    this.publish_status=ps
+  },
+  setreview:function(reiew){
+    this.review=reiew
+  },
+  setFamousBooks:function(fb){
+    this.Famous_books=fb
   }
 }
 //getting the name from the function
@@ -22,3 +37,18 @@ console.log(name)  //output=Hipple
 object.setName('Monika')
 const setname=object.getName().title;
 console.log(setname) //output=Monika
+object.setTitle('The end')
+const setname=object.getName().title;
+console.log(setname) //output=The end
+object.setYear(2019)
+const setname=object.getName().year;
+console.log(setname) //output=2019
+object.setPublishingStatus('Super')
+const setname=object.getName().publish_status;
+console.log(setname) //output=Super
+object.setreview('Awesome')
+const setname=object.getName().review;
+console.log(setname) //output=Awesome
+object.setFamousBooks('The legacy')
+const setname=object.getName().Famous_books;
+console.log(setname) //output=The legacy
