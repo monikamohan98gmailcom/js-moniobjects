c6bcf85affba79a7227b2558a78fd597e775cf82
monikamohan98gmailcom <monikamohan98@gmail.com>
2020-03-05 09:58:24 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//declaring object with key value pairs
 const object={
   title:'Hipple',
   author:'Paul Coelho',
@@ -5,16 +6,19 @@ const object={
   publish_status:'good',
   review:'good',
   Famous_books:'Kindle Digest',
+  //getting the key value pairs
   getName: function(){
     return this
   },
-  setName:function(name){
+  //setting the name with the arg value
+  setName:function(name){ 
     this.title=name
   }
 }
-
+//getting the name from the function
 const name=object.getName().title;
-console.log(name)
+console.log(name)  //output=Hipple
+//setting the name with arg passed to function
 object.setName('Monika')
 const setname=object.getName().title;
-console.log(setname)
+console.log(setname) //output=Monika
